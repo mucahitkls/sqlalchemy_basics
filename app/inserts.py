9b3f33efe857aaa319766f143eb4c138a1fade05
mücahit keleş <mchtttkls@gmail.com>
2023-12-22 12:00:02 +0100

    actor schema added

--- app/inserts.py
+++ app/inserts.py
@@ -15,23 +15,23 @@ mission_impo = Movie("Mission Impossible", date(1996, 8, 23))
 
 # 5 - create actors
 orlando_bloom = Actor('Orlando Bloom', date(1977, 1, 13))
-tom_criuse = Actor('Tom Criuse', date(1962, 7, 3))
+tom_cruise = Actor('Tom cruise', date(1962, 7, 3))
 ving_rhames = Actor('Ving Rhames', date(1959, 5, 12))
 
 # 6 - add actors to movies
 lotr.actors = [orlando_bloom]
-last_samurai.actors = [tom_criuse]
-mission_impo.actors = [tom_criuse, ving_rhames]
+last_samurai.actors = [tom_cruise]
+mission_impo.actors = [tom_cruise, ving_rhames]
 
 # 7 - add contact details to actors
 orlando_contact = ContactDetails("415 555 2671", "Burbank, CA", orlando_bloom)
-tom_contact = ContactDetails("423 555 5623", "Glendale, CA", tom_criuse)
-tom_contact_2 = ContactDetails("421 444 2323", "West Hollywood, CA", tom_criuse)
+tom_contact = ContactDetails("423 555 5623", "Glendale, CA", tom_cruise)
+tom_contact_2 = ContactDetails("421 444 2323", "West Hollywood, CA", tom_cruise)
 ving_contact = ContactDetails("421 333 9428", "Glendale, CA", ving_rhames)
 
 # 8 - create stuntmen
 orlando_stuntman = Stuntman("John Doe", True, orlando_bloom)
-tom_stuntman = Stuntman("John Roe", True, tom_criuse)
+tom_stuntman = Stuntman("John Roe", True, tom_cruise)
 ving_stuntman = Stuntman("Richard Roe", True, ving_rhames)
 
 # 9 - persists data
